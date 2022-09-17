1cced7dd9565e8a4d4df6a5f3953cbc96fe5e9b1
Mihir Savaliya <86355310+Mihir-Savaliya@users.noreply.github.com>
2022-09-17 22:39:57 +0530

    Add files via upload

---
+++ MAIN.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<LOGO.h>
+#include<Login1.h>
+#include<conio.h>
+#include<string.h>
+#include<stdio.h>
+#include<report.h>
+#include<graphics.h>
+#include<time.h>
+
+int main()
+{
+    clrscr();
+    textbackground(7);
+    Logo();
+    delay(1200);
+    login1();
+    report();
+    return 0;
+}
